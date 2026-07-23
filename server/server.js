78c62be220c7edb0ab8bd90283cc64ebe45f96a1
umran mihyo <s6662235@gmail.com>
2026-07-23 15:25:05 +0300

    fix:

--- server/server.js
+++ server/server.js
@@ -14,7 +14,7 @@ app.use(
     origin: [
       "http://localhost:5173",
       "http://127.0.0.1:5173",
-      "https://clinic-fullstack-chi.vercel.app/",
+      "https://clinic-fullstack-chi.vercel.app",
     ],
     methods: ["GET", "POST", "PUT", "DELETE"],
     credentials: true,
